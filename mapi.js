3c1ef3f3c03f80cae30f024027edc7a44458b681
Josh Wilsdon <jwilsdon@joyent.com>
2011-04-05 11:30:00 -0700

    [HVM-18] Add support to booter for handling HVM-* compute node images.

--- mapi.js
+++ mapi.js
@@ -57,7 +57,48 @@ getBootParams = function(mac, cb) {
 }
 exports.getBootParams = getBootParams;
 
-buildMenuLst = function(mac, c) {
+buildHVMMenuLst = function(mac, c) {
+    var kargs_arr = [];
+    for (a in c.kernel_args) {
+      kargs_arr.push(a + '=' + c.kernel_args[a]);
+    }
+    kargs_arr.push("nosplash");
+    kargs_arr.push("rw");
+    var kargs = kargs_arr.join(' ');
+    var module = '/os/' + c.platform + '/platform/initrd';
+    var kernel = '/os/' + c.platform + '/platform/vmlinuz';
+    var extra = '';
+    for (var n in c.nic_tags) {
+      extra += " " + n + "_nic=" + c.nic_tags[n];
+    }
+
+    return (
+    [ "default=0"
+    , "timeout=5"
+    , "min_mem64 1024"
+    , "color grey/blue black/blue"
+    , "splashimage=/joybadger.xpm.gz"
+    , ""
+    , "title HVM Live 64-bit"
+    , "  kernel " + kernel + " " + kargs + extra
+    , "  initrd " + module
+    , ""
+    , "title HVM Live 64-bit Serial (ttyb)"
+    , "  kernel " + kernel + " " + kargs + ' console=tty0 console=ttyS1,115200n8' + extra
+    , "  initrd " + module
+    , ""
+    , "title HVM Live 64-bit Serial (ttya)"
+    , "  kernel " + kernel + " " + kargs + ' console=tty0 console=ttyS0,115200n8' + extra
+    , "  initrd " + module
+    , ""
+    , "title HVM Live 64-bit Rescue (no mounting LVM)"
+    , "  kernel " + kernel + " " + kargs + extra + ' noimport=true'
+    , "  initrd " + module
+    , ""
+    ].join('\n'));
+}
+
+buildSmartOSMenuLst = function(mac, c) {
     var kargs_debug = 'prom_debug=true,map_debug=true,kbm_debug=true';
     var kargs_arr = [];
     for (a in c.kernel_args) {
@@ -108,8 +149,16 @@ buildMenuLst = function(mac, c) {
     , ""
     ].join('\n'));
 }
-exports.buildMenuLst = buildMenuLst
 
+buildMenuLst = function(mac, c) {
+  if (c.platform.substr(0,4) === "HVM-") {
+    return buildHVMMenuLst(mac, c);
+  } else {
+    return buildSmartOSMenuLst(mac, c);
+  }
+}
+
+exports.buildMenuLst = buildMenuLst;
 
 exports.writeMenuLst = function (mac, dir, cb) {
   var filename = dir + '/menu.lst.01' + mac.replace(/:/g, '').toUpperCase();
